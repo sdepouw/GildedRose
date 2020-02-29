03a18ff277350ed350775a6cae4d4767ab5bf52e
Scott DePouw <scott@ardalis.com>
2020-02-28 19:07:15 -0500

    Remove Max Quality limitation from Sulfuras Item.

--- Inventory/SulfurasItem.cs
+++ Inventory/SulfurasItem.cs
@@ -3,6 +3,7 @@ namespace GildedRose.Inventory
     public class SulfurasItem : GildedItem
     {
         public SulfurasItem(Item item) : base(item) { }
+        protected override int MaxQuality => int.MaxValue;
 
         protected override void UpdateQuality() { }
         protected override void UpdateSellIn() { }
